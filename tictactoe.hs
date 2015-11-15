d66920ba7b863b5681d197aa3eae2dbe492ebde4
Mark Karavan <mark.karavan@gmail.com>
2015-11-15 17:59:08 -0500

    adds enterMove, endGame and detectWin

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,5 +1,9 @@
 import Control.Monad
 
+data Square = A | B | C | D | E | F | G | H | I | X | O deriving (Show, Read)
+type Row = [Square]
+type Board = [Row]
+
 main = do
     putStrLn "Let's play some motherfuckin tic tac toe!!!"
     putStrLn "Yeeeaaaaaahh!!!"
@@ -23,15 +27,34 @@ onePlayerGameLoop boardstate = do
     
 twoPlayerMode = do
     putStrLn "Two players"
-    gameLoop (emptyBoard) 2 1 -- start game loop with empty board, two players, initialize to player one
+    gameLoop emptyBoard "2" "1" -- start game loop with empty board, two players, initialize to player one
 
 gameLoop boardstate players currentPlayer = do
 	putStrLn $ show boardstate ++ show players ++ show currentPlayer
+	case detectWin boardstate of "1" -> endgame "1"
+	                             "2" -> endgame "2"
+	                             "0" -> enterMove boardstate players currentPlayer
 
+enterMove boardstate players currentPlayer
+    | players == "2" = do
+        putStrLn "Inside enterMove"
+        putStrLn ("Boardstate: " ++ show boardstate)
+        putStrLn ("Players: " ++ players)
+        putStrLn ("currentPlayer: " ++ currentPlayer)
+        gameLoop boardstate players (if currentPlayer == "1" then "2" else "1")
 
-data Square = A | B | C | D | E | F | G | H | I | X | O deriving (Show, Read)
-type Row = [Square]
-type Board = [Row]
+endgame player = do
+    putStrLn ("The game is over, and player " ++ show player ++ " wins!")
+    putStrLn "The other guy is a loser lol"
+    putStrLn "Want to play again? (y/n)"
+    again <- getLine
+    if again `elem` ["y", "Y", "yes", "Yes", "YES"] 
+    	then gameSelect 
+    	else do
+    	    putStrLn "Goodbye"
+
+detectWin boardstate = "0"
+
 
 emptyBoard :: Board
 emptyBoard = [[A,B,C],[D,E,F],[G,H,I]]
